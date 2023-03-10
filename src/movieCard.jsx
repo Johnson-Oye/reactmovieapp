e0f4bee7b3af04a17fd9b50bef4b417bfd816b45
olax <olax123@gmail.com>
2023-03-10 15:59:09 +0100

    second commit

---
+++ src/movieCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'; 
+ 
+const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => { 
+  return ( 
+    <div key={imdbID}> 
+      <div> 
+        <p>{Year}</p> 
+      </div> 
+ 
+      <div> 
+        <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} width="300px" alt={Title} /> 
+      </div> 
+ 
+      <div> 
+        <span>{Type}</span> 
+        <h2>{Title}</h2> 
+      </div> 
+    </div> 
+  ); 
+} 
+ 
+export default MovieCard;
